1b3f89dec0ba855d24d4b92a6ec8c649c44debc5
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2024-09-23 11:03:19 +0200

    maj

--- project_docker_init/auth_entry_point.sh
+++ project_docker_init/auth_entry_point.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Attendre que la base de données soit prête
-until nc -z -v -w30 auth_db 3306
-do
-  echo "Waiting for database connection..."
-  sleep 5
-done
+# until nc -z -v -w30 auth_db 3306
+# do
+#   echo "Waiting for database connection..."
+#   sleep 5
+# done
 
 # Exécuter les migrations
 php bin/console doctrine:migrations:migrate --no-interaction
